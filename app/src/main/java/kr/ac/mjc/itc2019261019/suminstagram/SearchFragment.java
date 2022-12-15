0c55a6df96895a7b2d762613edf2220bf8c50356
a5a030 <ssuddie0726@naver.com>
2022-12-15 19:38:31 +0900

    팔로우 이전 간단 검색 구현

--- app/src/main/java/kr/ac/mjc/itc2019261019/suminstagram/SearchFragment.java
+++ app/src/main/java/kr/ac/mjc/itc2019261019/suminstagram/SearchFragment.java
@@ -8,12 +8,33 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.ac.mjc.itc2019261019.suminstagram.model.User;
 
 public class SearchFragment extends Fragment {
+
+    private RecyclerView searchRv;
+    private UserAdapter userAdapter;
+    private List<User> mUsers;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        searchRv = view.findViewById(R.id.search_rv);
+        searchRv.setHasFixedSize(true);
+        searchRv.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), mUsers);
+        searchRv.setAdapter(userAdapter);
+
+        return view;
     }
 
     @Override
