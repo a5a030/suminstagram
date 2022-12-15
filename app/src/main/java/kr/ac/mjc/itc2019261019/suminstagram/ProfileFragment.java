91684a1ec37fe61b4f1585de4f8001ce1bb73386
a5a030 <ssuddie0726@naver.com>
2022-12-15 22:51:24 +0900

    프로필 제작 중

--- app/src/main/java/kr/ac/mjc/itc2019261019/suminstagram/ProfileFragment.java
+++ app/src/main/java/kr/ac/mjc/itc2019261019/suminstagram/ProfileFragment.java
@@ -4,12 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfileFragment extends Fragment {
+    ImageView profileIv, optionsIv;
+    TextView postsTv, followersTv, followingTv,  usernameTv, fullNameTv, bioTv;
+    Button editBtn;
+
+    FirebaseUser firebaseUser;
+    String profileId;
+    ImageButton photos, savedPhotos;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +34,9 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        profileIv = view.findViewById(R.id.profile_iv);
     }
 }
